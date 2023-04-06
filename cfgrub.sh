b240bd7ea0cd435eb92e80fc6698ec8e503f2d33
Elliot Alderson <richard.saifier.87@gmail.com>
2023-04-06 18:10:09 +0300

    Changed the name of the first item from download to boot; Added a lot of handling of various errors, introduced input validation in some places.; Removed the version number from the graphics, added it as a separate line; Moved the output of the menu into a separate function.

--- cfgrub.sh
+++ cfgrub.sh
@@ -1,6 +1,6 @@
 #AUTHOR
 #	Written by Samir Kasumov.
-#        Version 1.3
+#        Version 1.4
 
 #REPORTING BUGS
 #	Report any bugs to https://vk.com/samir_kasumov
@@ -16,36 +16,19 @@
 CHECK_ROOT
 
 echo "
-            __                  _      __     __            _             
-       ___ / _| __ _ _ __ _   _| |__   \ \   / /__ _ __ ___(_) ___  _ __  
-      / __| |_ / _\` | '__| | | | '_ \   \ \ / / _ \ '__/ __| |/ _ \| '_ \ 
-     | (__|  _| (_| | |  | |_| | |_) |   \ V /  __/ |  \__ \ | (_) | | | |
-      \___|_|  \__, |_|   \__,_|_.__/     \_/ \___|_|  |___/_|\___/|_| |_|
-               |___/                                                      
-                                   _   _____ 
-                                  / | |___ / 
-                                  | |   |_ \ 
-                                  | |_ ___) |
-                                  |_(_)____/ 
+       __                  _     
+  ___ / _| __ _ _ __ _   _| |__  
+ / __| |_ / _\` | '__| | | | '_ \ 
+| (__|  _| (_| | |  | |_| | |_) |
+ \___|_|  \__, |_|   \__,_|_.__/ 
+          |___/   
 "
 
-
+echo "cfgrub version 1.4"
 PRINT_GRUB_VERSION
 
 
-cat << EOF
-1. Change download point
-2. Change timeout for user action
-3. Change screen resolution
-4. Change show "recovery mode" items
-5. Change background image
-6. Change display of kernel messages and distribution logo
-7. Interactive mode with a pass through all items
-8. Show current configuration
-9. Сreate a configuration backup
-10. Use a backup to replace the current configuration
-11. Exit
-EOF
+SHOW_MENU
 
 
 read -p "Select an item from the menu: " MENU_RESPONSE
@@ -53,7 +36,7 @@ read -p "Select an item from the menu: " MENU_RESPONSE
 
 case $MENU_RESPONSE in
 	1) 
-	   	SET_DOWNLOAD_POINT
+	   	SET_BOOT_POINT
 	   ;;
 	2) 
 	   	SET_TIMEOUT
@@ -71,7 +54,7 @@ case $MENU_RESPONSE in
 	   	SET_KERNEL_MESSG_AND_LOGO
 	   ;;
 	7) 
-		SET_DOWNLOAD_POINT
+		SET_BOOT_POINT
 		SET_TIMEOUT
 		SET_GFXMODE
 		SET_GRUB_DISABLE_RECOVERY
@@ -93,7 +76,7 @@ case $MENU_RESPONSE in
 		exit 0
 	    ;;
 	*) 
-		echo Invalid menu item entered
+		echo Invalid menu item entered.
 		exit 1
 		;;
 esac
